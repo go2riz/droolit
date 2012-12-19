8b049be57af8545853f5f2d222e21d26c40a5e4a
Rizwan <go2riz@gmail.com>
2012-12-19 18:44:34 +0500

    generic auth_token filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :check_auth_token
+  
   protected
   
   def set_api_response code, details=""
     @api_response = ApiMessage.where(code: code).first
     @api_response.details = details if @api_response.present?
   end
+  
+  def check_auth_token
+    @user = User.where(authentication_token: params[:auth_token]).first
+    
+    if @user.nil? || !@user.confirmed?
+      set_api_response("401", "Unauthorized access.")
+      render :template => "/devise/shared/authentication_failed"
+    end
+  end
+  
   
 end
