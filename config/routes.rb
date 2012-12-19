859d3413974f81afc23350c1eba1ab5052ffcdc8
Rizwan <go2riz@gmail.com>
2012-12-19 12:55:49 +0500

    Forgot Password API

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'resque/server'
 Droolitapi::Application.routes.draw do
   mount Resque::Server.new, :at => "/resque"
 
-  devise_for :users, :controllers => {:registrations => "registrations", :confirmations => "confirmations"}
+  devise_for :users, :controllers => {:registrations => "registrations", :confirmations => "confirmations", :passwords => "passwords"}
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
